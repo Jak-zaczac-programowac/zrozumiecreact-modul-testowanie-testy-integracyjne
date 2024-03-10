1088bde3b4f831a9ef83c4b5149b16f29a25471c
Kacper Sokołowski <kontakt@kaapa.pl>
2024-03-10 16:44:30 +0100

    Konfiguracja Jesta koniec

---
+++ tests/integration/button.spec.jsx
@@ -0,0 +1,7 @@
+import { Button } from "../../src/components/Button/Button";
+
+test("Button integration test", () => {
+    console.log("Tests run!");
+    const button = <Button />;
+    console.log(button);
+});
